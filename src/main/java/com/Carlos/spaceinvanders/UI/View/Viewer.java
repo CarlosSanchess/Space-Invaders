f6c52c5c1113130c7e9006535998c97a0718b589
Cruddle <up202107694@fe.up.pt>
2023-11-23 23:29:07 +0000

    Changes on Menu package

--- src/main/java/com/Carlos/spaceinvanders/UI/View/Viewer.java
+++ src/main/java/com/Carlos/spaceinvanders/UI/View/Viewer.java
@@ -1,4 +1,24 @@
 package com.Carlos.spaceinvanders.UI.View;
 
-public class Viewer {
+
+
+import com.Carlos.spaceinvanders.GUI.LanternaGui;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.io.IOException;
+
+public abstract class Viewer <T>{  // Este viewer, da display do jogo por completo, e do menu.
+
+    private final T model;
+
+    public Viewer(T model){
+        this.model = model;
+    }
+
+    void draw(LanternaGui GUI) throws IOException {
+        GUI.screenClear();
+        drawGame(GUI);
+        GUI.screenRefresh();
+    }
+    abstract void drawGame(LanternaGui GUI);
 }
