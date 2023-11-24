fed20dbed969e5cbfb89d71179b5652994a1092c
arcinard <rebelo0912joao@outlook.com>
2023-11-24 20:57:18 +0000

    Lanterna GUI a desenhar corretamente  o menu com core de fundo

--- src/main/java/com/Carlos/spaceinvanders/GUI/LanternaGui.java
+++ src/main/java/com/Carlos/spaceinvanders/GUI/LanternaGui.java
@@ -14,78 +14,79 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 import java.util.List;
 
-
 public class LanternaGui {
 
     private TextGraphics graphics;
     private Screen screen;
-    public LanternaGui(int widht, int height) throws IOException {
-        Terminal terminal = createTerminal(widht, height);
+
+    public LanternaGui(int width, int height) throws IOException {
+        Terminal terminal = createTerminal(width, height);
         screen = createScreen(terminal);
         graphics = createGraphics(screen);
     }
 
-    private Terminal createTerminal(int width, int height) throws  IOException {
+    private Terminal createTerminal(int width, int height) throws IOException {
         TerminalSize terminalSize = new TerminalSize(width, height);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-
-        Terminal terminal  = terminalFactory.createTerminal();
-
-        return terminal;
+        return terminalFactory.createTerminal();
     }
 
+    private Screen createScreen(Terminal terminal) throws IOException {
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
 
-        private Screen createScreen(Terminal terminal) throws IOException {
-            final Screen screen;
-            screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-            return screen;
-        }
-
-        private TextGraphics createGraphics(Screen screen){
-            TextGraphics graphics = screen.newTextGraphics();
-            return graphics;
-        }
+    private TextGraphics createGraphics(Screen screen) {
+        return screen.newTextGraphics();
+    }
 
     public TextGraphics getGraphics() {
         return graphics;
     }
 
-    public void drawText(PositionModel position, String string, TextColor.RGB rgbColor){ // Testar esta função
-            graphics.setForegroundColor(rgbColor);
-            graphics.putString(position.getX(), position.getY(), string);
+    public void drawText(PositionModel position, String string, TextColor.RGB rgbColor) {
+        graphics.setForegroundColor(rgbColor);
+        graphics.putString(position.getX(), position.getY(), string);
     }
+
     public void screenClear() {
         screen.clear();
-        TextGraphics graphics = screen.newTextGraphics();
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#4B0082")); 
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#4B0082"));
         graphics.fillRectangle(new TerminalPosition(0, 0), screen.getTerminalSize(), ' ');
     }
+
     public void screenRefresh() throws IOException {
         screen.refresh();
     }
-    public void drawTitle(String string){
+
+    public void renderMenu(MenuModel menuModel, String title) throws IOException {
+        drawTitle(title);
+        drawEntries(menuModel);
+        screenRefresh();
+    }
+
+    public void drawTitle(String string) {
         screenClear();
-        String [] lines = string.split("\n");
+        String[] lines = string.split("\n");
         int y = 0;
         for (String line : lines) {
             drawText(new PositionModel(7, y), line, new TextColor.RGB(178, 73, 210));
             y++;
         }
     }
-    public void drawEntries(MenuModel menuModel){
-        screenClear();
+
+    public void drawEntries(MenuModel menuModel) {
         List<String> entries = menuModel.getEntries();
         int y = 10;
         for (String entry : entries) {
             TextColor.RGB color = menuModel.getColor(entry);
-            graphics.setBackgroundColor(TextColor.Factory.fromString("#4B0082")); 
+            graphics.setBackgroundColor(TextColor.Factory.fromString("#4B0082"));
             drawText(new PositionModel(33, y), entry, color);
             y += 3;
         }
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#000000")); 
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
     }
 }
