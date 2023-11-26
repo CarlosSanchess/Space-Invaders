95b13507de06b31b0e3c97ad4d256479ab5d0aa4
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:07:54 +0000

    ArenaTest e BulletTest models
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/model/models/ArenaTest.java
+++ src/test/java/com/Carlos/spaceinvaders/model/models/ArenaTest.java
@@ -1,2 +1,53 @@
-package com.Carlos.spaceinvaders.model.models;public class ArenaTest {
+package com.Carlos.spaceinvaders.model.models;
+import com.Carlos.spaceinvanders.model.models.ArenaModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+
+public class ArenaTest {
+
+    private ArenaModel arena;
+
+    @BeforeEach
+    public void setUp() throws InterruptedException {
+        arena = new ArenaModel(100, 80);
+    }
+
+    @Test
+    public void testGetWidth() {
+        assertEquals(100, arena.getWidth());
+    }
+
+    @Test
+    public void testGetHeight() {
+        assertEquals(80, arena.getHeight());
+    }
+
+    @Test
+    public void testGetPlayer() {
+        assertNotNull(arena.getPlayer());
+    }
+
+    @Test
+    public void testGetActiveBullets() {
+        assertNotNull(arena.getActiveBullets());
+    }
+
+    @Test
+    public void testGetActiveMonsters() {
+        assertNotNull(arena.getActiveMonsters());
+    }
+
+    @Test
+    public void testGetWalls() {
+        assertNotNull(arena.getWalls());
+    }
+
+    @Test
+    public void testGetScore() {
+        assertNotNull(arena.getScore());
+    }
 }
--- src/test/java/com/Carlos/spaceinvaders/model/models/BulletTest.java
+++ src/test/java/com/Carlos/spaceinvaders/model/models/BulletTest.java
@@ -1,2 +1,35 @@
-package com.Carlos.spaceinvaders.model.models;public class BulletTest {
+package com.Carlos.spaceinvaders.model.models;
+
+
+import com.Carlos.spaceinvanders.model.models.BulletModel;
+import com.Carlos.spaceinvanders.model.models.PositionModel;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BulletTest {
+    private BulletModel bullet;
+
+    @BeforeEach
+    public void setUp() {
+        PositionModel position = new PositionModel(0, 0);
+        int speed = 5;
+        boolean direction = true;
+        bullet = new BulletModel(position, speed, direction);
+    }
+
+    @Test
+    public void testMove() {
+        bullet.move();
+        assertEquals(-5, bullet.getPosition().getY());
+    }
+
+    @Test
+    public void testIsActive() {
+        assertTrue(bullet.isActive());
+
+        bullet.getPosition().setY(-10);
+        assertFalse(bullet.isActive());
+    }
 }
