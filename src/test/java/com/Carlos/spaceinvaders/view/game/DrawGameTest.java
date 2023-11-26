4a363e1b8e7e5d4ba600d0e88df99388a96b97f7
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:33:00 +0000

    DrawGameTest e DrawMenuTest
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawGameTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawGameTest.java
@@ -1,4 +1,37 @@
 package com.Carlos.spaceinvaders.view.game;
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.ArenaModel;
+import com.Carlos.spaceinvanders.model.models.PositionModel;
+import com.Carlos.spaceinvanders.model.models.ScoreModel;
+import com.Carlos.spaceinvanders.view.game.DrawGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
 
 public class DrawGameTest {
+
+    private DrawGame drawGame;
+    private ArenaModel arenaModel;
+    private LanternaGui gui;
+
+    @BeforeEach
+    public void setUp() {
+        arenaModel = Mockito.mock(ArenaModel.class);
+        PositionModel position = new PositionModel(0, 0);
+        ScoreModel scoreModel = new ScoreModel(position);
+        Mockito.when(arenaModel.getScore()).thenReturn(scoreModel);
+        gui = Mockito.mock(LanternaGui.class);
+        drawGame = new DrawGame(arenaModel);
+}
+
+    @Test
+    public void testDraw() {
+        drawGame.draw(gui);
+
+        verify(arenaModel).getActiveMonsters();
+        verify(arenaModel).getActiveBullets();
+        verify(arenaModel).getWalls();
+        verify(arenaModel).getScore();
+    }
 }
--- src/test/java/com/Carlos/spaceinvaders/view/menu/DrawMenuTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/menu/DrawMenuTest.java
@@ -1,4 +1,37 @@
 package com.Carlos.spaceinvaders.view.menu;
 
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.MenuModel;
+import com.Carlos.spaceinvanders.view.menu.DrawMenu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
 public class DrawMenuTest {
+
+    @Mock
+    private LanternaGui guiMock;
+
+    private DrawMenu drawMenu;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        MenuModel menuModel = new MenuModel();
+        drawMenu = new DrawMenu(menuModel);
+    }
+
+    @Test
+    public void testDraw() {
+        MenuModel menuModel = drawMenu.getModel();
+        menuModel.setText("Space Invaders");
+
+        drawMenu.draw(guiMock);
+
+        verify(guiMock).drawTitle("Space Invaders");
+        verify(guiMock).drawEntries(menuModel);
+    }
 }
