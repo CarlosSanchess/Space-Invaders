53273e6e907c19295fe0ba70382648d2754df990
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:31:14 +0000

    DrawWallTest e DrawOutlineTest
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawOutlineTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawOutlineTest.java
@@ -1,4 +1,44 @@
 package com.Carlos.spaceinvaders.view.game;
 
-public class DrawOutlineTest {
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.WallModel;
+import com.Carlos.spaceinvanders.view.game.DrawOutline;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class DrawOutlineTest {
+
+    @Mock
+    private LanternaGui mockGui;
+
+    private DrawOutline drawOutline;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        List<WallModel> walls = new ArrayList<>();
+        drawOutline = new DrawOutline(walls);
+    }
+
+    @Test
+    void draw_ShouldDrawEachWallModel() {
+        WallModel wallModel1 = mock(WallModel.class);
+        WallModel wallModel2 = mock(WallModel.class);
+        List<WallModel> walls = new ArrayList<>();
+        walls.add(wallModel1);
+        walls.add(wallModel2);
+
+        DrawOutline drawOutline = new DrawOutline(walls);
+
+        drawOutline.draw(mockGui);
+
+        verify(mockGui, times(walls.size())).drawText(any(), any(), any());
+    }
 }
