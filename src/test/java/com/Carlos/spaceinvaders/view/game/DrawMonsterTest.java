535c33cfd7a04247030b2e8796782a850be8c051
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:28:13 +0000

    DrawMonster e DrawMonstersTest(viewers precisam mudança)
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawMonsterTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawMonsterTest.java
@@ -1,4 +1,26 @@
 package com.Carlos.spaceinvaders.view.game;
 
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.MonsterModel;
+import com.Carlos.spaceinvanders.model.models.PositionModel;
+import com.Carlos.spaceinvanders.view.game.DrawMonster;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class DrawMonsterTest {
+
+    @Test
+    public void testDraw() {
+        LanternaGui guiMock = mock(LanternaGui.class);
+
+        MonsterModel monsterModel = new MonsterModel(new PositionModel(1,1), 10, 10);
+
+        DrawMonster drawMonster = new DrawMonster(monsterModel);
+
+        drawMonster.draw(guiMock);
+
+        verify(guiMock).drawText(monsterModel.getPosition(), "M", new TextColor.RGB(255, 0, 0));
+    }
 }
