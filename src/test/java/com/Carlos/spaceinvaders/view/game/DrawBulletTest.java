c56c90c45771c0602d2e8f601e060af8bcd53891
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:27:01 +0000

    DrawBulletTest e DrawBulletsTest (os viewers destas duas classes precisam de mudanças)
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawBulletTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawBulletTest.java
@@ -1,4 +1,27 @@
 package com.Carlos.spaceinvaders.view.game;
 
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.BulletModel;
+import com.Carlos.spaceinvanders.model.models.PositionModel;
+import com.Carlos.spaceinvanders.view.game.DrawBullet;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
 public class DrawBulletTest {
+
+    @Test
+    public void testDraw() {
+        LanternaGui guiMock = mock(LanternaGui.class);
+
+        PositionModel positionModel = new PositionModel(0, 0);
+
+        BulletModel bulletModel = new BulletModel(positionModel, 1, true);
+
+        DrawBullet drawBullet = new DrawBullet(bulletModel);
+
+        drawBullet.draw(guiMock);
+
+        verify(guiMock).drawText(bulletModel.getPosition(), "|", new TextColor.RGB(255, 0, 0));
+    }
 }
--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawBulletsTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawBulletsTest.java
@@ -1,4 +1,47 @@
 package com.Carlos.spaceinvaders.view.game;
 
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.BulletModel;
+import com.Carlos.spaceinvanders.model.models.PositionModel;
+import com.Carlos.spaceinvanders.view.game.DrawBullets;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 public class DrawBulletsTest {
+
+    @Mock
+    private LanternaGui guiMock;
+
+    private DrawBullets drawBullets;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        List<BulletModel> bullets = new ArrayList<>();
+        PositionModel position1 = new PositionModel(1, 1);
+        BulletModel bullet1 = new BulletModel(position1, 5, true);
+
+        PositionModel position2 = new PositionModel(2, 2);
+        BulletModel bullet2 = new BulletModel(position2, 3, false);
+
+        bullets.add(bullet1);
+        bullets.add(bullet2);
+        drawBullets = new DrawBullets(bullets);
+    }
+
+    @Test
+    public void testDraw() {
+        drawBullets.draw(guiMock);
+
+        verify(guiMock, times(2)).drawText(any(),any(),any());
+    }
 }
