fd92c3b0c9abb826f1f5c625a6e75d59bced0e48
arcinard <rebelo0912joao@outlook.com>
2023-11-26 18:50:36 +0000

    Removi comment do ArenaModel

--- src/main/java/com/Carlos/spaceinvaders/model/models/ArenaModel.java
+++ src/main/java/com/Carlos/spaceinvaders/model/models/ArenaModel.java
@@ -65,30 +65,3 @@ public class ArenaModel implements Model {
         return score;
     }
 }
-
-    //Tirar daqui o processKey, dar get na arena e mover a partir dai
-
-    /*
-    public void processKey(KeyStroke key) throws IOException, InterruptedException { // Aqui ou no controls package ou no game
-
-            switch (key.getKeyType()) {
-                case ArrowLeft:
-                    player.moveLeft(player.getPosition());
-                    break;
-                case ArrowRight:
-                    player.moveRight(player.getPosition());
-                    break;
-                case Backspace: //ADD CANSHOOT? nerfar a arma, possivle
-                    activeBullets.add(this.player.playerShoot()); //Ok
-                    break;
-            }
-        if(key.getCharacter() != null) {
-            switch (key.getCharacter()) {
-                case 'a','A'-> player.moveLeft(player.getPosition());
-                case 'd','D' -> player.moveRight(player.getPosition());
-
-            }
-        }
-    }
-
-     */
