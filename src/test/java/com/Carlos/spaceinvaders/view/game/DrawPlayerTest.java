4358422f186dd28d20ef9d4268dff3f5a69963e6
arcinard <rebelo0912joao@outlook.com>
2023-11-26 16:32:34 +0000

    DrawPlayerTest e DrawScoreTest
    Co-authored-by: CarlosSanchess up202107694@fe.up.pt

--- src/test/java/com/Carlos/spaceinvaders/view/game/DrawPlayerTest.java
+++ src/test/java/com/Carlos/spaceinvaders/view/game/DrawPlayerTest.java
@@ -1,4 +1,29 @@
 package com.Carlos.spaceinvaders.view.game;
 
+import com.Carlos.spaceinvanders.gui.LanternaGui;
+import com.Carlos.spaceinvanders.model.models.PlayerModel;
+import com.Carlos.spaceinvanders.view.game.DrawPlayer;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+
 public class DrawPlayerTest {
+    private DrawPlayer drawPlayer;
+    private PlayerModel playerModel;
+    private LanternaGui gui;
+
+    @BeforeEach
+    public void setUp() {
+        playerModel = Mockito.mock(PlayerModel.class);
+        gui = Mockito.mock(LanternaGui.class);
+        drawPlayer = new DrawPlayer(playerModel);
+    }
+
+    @Test
+    public void testDraw() {
+        drawPlayer.draw(gui);
+        verify(gui).drawText(playerModel.getPosition(), "P", new TextColor.RGB(0, 255, 0));
+    }
 }
