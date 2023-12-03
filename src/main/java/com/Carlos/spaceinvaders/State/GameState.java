f4f0bf4a6e2abe70040a68e0c25157e70e689b6a
arcinard <rebelo0912joao@outlook.com>
2023-12-03 12:45:25 +0000

    código a correr

--- src/main/java/com/Carlos/spaceinvaders/State/GameState.java
+++ src/main/java/com/Carlos/spaceinvaders/State/GameState.java
@@ -1,6 +1,7 @@
 package com.Carlos.spaceinvaders.State;
 
 import com.Carlos.spaceinvaders.controller.game.GameController;
+import com.Carlos.spaceinvaders.controller.game.MonsterControllerFactory;
 import com.Carlos.spaceinvaders.gui.LanternaGui;
 import com.Carlos.spaceinvaders.model.models.ArenaModel;
 import com.Carlos.spaceinvaders.view.game.DrawGame;
@@ -10,17 +11,20 @@ import java.io.IOException;
 public class GameState extends State<ArenaModel> {
 
 
-    DrawGame drawGame;
-    GameController gameController;
+    private DrawGame drawGame;
+    private GameController gameController;
+    private MonsterControllerFactory monsterControllerFactory;
     public GameState(ArenaModel arenaModel){
         super(arenaModel);
+        monsterControllerFactory = new MonsterControllerFactory(super.getModel().getWidth());
     }
 
 
     @Override
     public void step(String Key, LanternaGui GUI) throws IOException {
         this.drawGame = new DrawGame(super.getModel());
-        this.gameController = new GameController(super.getModel());
+
+        this.gameController = new GameController(super.getModel(), monsterControllerFactory);
         drawGame.lanternaDraw(GUI);
         gameController.toDo(Key);
     }
