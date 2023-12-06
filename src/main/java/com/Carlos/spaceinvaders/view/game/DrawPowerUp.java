7505ee9c7c90777223d1c7e1c62dc47e0898bb32
arcinard <rebelo0912joao@outlook.com>
2023-12-06 16:19:12 +0000

    desenho dos powerups

--- src/main/java/com/Carlos/spaceinvaders/view/game/DrawPowerUp.java
+++ src/main/java/com/Carlos/spaceinvaders/view/game/DrawPowerUp.java
@@ -16,9 +16,9 @@ public class DrawPowerUp extends Viewer<PowerUp> implements DrawElement {
         PowerUp powerUp = getModel();
         if(powerUp.isActive() != 2){
             if(powerUp.getPowerUpType() == PowerUp.PowerUpType.HealthBoost)
-                GUI.drawText(powerUp.getPosition(),"H",new TextColor.RGB(200,100,0));
+                GUI.drawText(powerUp.getPosition(),"+",new TextColor.RGB(0,255,0));
             if(powerUp.getPowerUpType() == PowerUp.PowerUpType.ScoreBoost)
-                GUI.drawText(powerUp.getPosition(),"S",new TextColor.RGB(255,255,0));
+                GUI.drawText(powerUp.getPosition(),"★",new TextColor.RGB(255,255,0));
         }
     }
 }
