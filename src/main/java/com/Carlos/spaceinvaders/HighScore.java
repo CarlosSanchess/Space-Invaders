ca8a2570b7614b7df74e2cfac3649897833096bc
arcinard <rebelo0912joao@outlook.com>
2023-12-06 14:01:10 +0000

    HighScore a funcionar

--- src/main/java/com/Carlos/spaceinvaders/HighScore.java
+++ src/main/java/com/Carlos/spaceinvaders/HighScore.java
@@ -3,23 +3,20 @@ package com.Carlos.spaceinvaders;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.*;
+import java.util.Objects;
 
 public class HighScore {
     private int score;
-    private static String filePath = "HighScore.csv";
+    private static String filePath = "/HighScore.csv";
+    private static String absolutePath = System.getProperty("user.dir") + "/src/main/resources/HighScore.csv";
 
     public HighScore(int score) {
         this.score = score;
     }
 
-    public int getScore() {
-        return score;
-    }
-
     public static int loadHighScore() {
         int highScore = 0;
-        try (InputStream in = HighScore.class.getResourceAsStream("/" + filePath);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(HighScore.class.getResourceAsStream(filePath))))) {
             String line;
             if ((line = reader.readLine()) != null) {
                 highScore = Integer.parseInt(line);
@@ -29,9 +26,8 @@ public class HighScore {
         }
         return highScore;
     }
-
     public static void saveHighScore(int score) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, false))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(absolutePath, false))) {
             writer.println(score);
         } catch (IOException e) {
             System.err.println("Error saving high score: " + e.getMessage());
