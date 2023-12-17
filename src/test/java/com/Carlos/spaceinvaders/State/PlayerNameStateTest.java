13b02e3b80f32eb64a8ae0cfafe49d74b9498007
arcinard <rebelo0912joao@outlook.com>
2023-12-17 18:06:55 +0000

    PlayerNameStateTest

---
+++ src/test/java/com/Carlos/spaceinvaders/State/PlayerNameStateTest.java
@@ -0,0 +1,49 @@
+package com.Carlos.spaceinvaders.State;
+
+import com.Carlos.spaceinvaders.Game;
+import com.Carlos.spaceinvaders.controller.menu.PlayerNameController;
+import com.Carlos.spaceinvaders.gui.LanternaGui;
+import com.Carlos.spaceinvaders.model.models.PlayerNameModel;
+import com.Carlos.spaceinvaders.view.menu.DrawPlayerName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+class PlayerNameStateTest {
+    @Mock
+    private PlayerNameModel playerNameModel;
+    @Mock
+    private DrawPlayerName drawPlayerNameModel;
+    @Mock
+    private PlayerNameController playerNameController;
+    @Mock
+    private LanternaGui gui;
+    @Mock
+    private Game game;
+
+    private PlayerNameState playerNameState;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        playerNameState = new PlayerNameState(playerNameModel);
+        playerNameState.setDrawPlayerNameModel(drawPlayerNameModel);
+        playerNameState.setPlayerNameController(playerNameController);
+    }
+
+    @Test
+    void testStep() throws IOException {
+        String key = "A";
+        long time = 1000L;
+
+        playerNameState.step(key, gui, time, game);
+
+        verify(drawPlayerNameModel, times(1)).draw(gui);
+        verify(playerNameController, times(1)).toDo(game, key, time);
+    }
+}
